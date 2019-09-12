a3a1793e922d8102db638ea01e3f6a6678068ed1
Martin Ritter <maritter@umd.edu>
2019-09-12 13:48:33 -0400

    added comments and out of range error handling to combineCombs and findEl codes

--- Data_Analysis/UserTools/helperfuncs/combineCombs.m
+++ Data_Analysis/UserTools/helperfuncs/combineCombs.m
@@ -1,14 +1,24 @@
+% This function combines two comb sweeps into a single comb for further
+% analysis. The function expects arrays of frequency and amplitudes for
+% each comb, finds the overlap region and concatenates the three regions
+% together (comb1, comb1+comb2, comb2). 
+
 function [freq, spec] = combineCombs(freqaxisp1,amp1, freqaxisp2, amp2)
 
+% Get start/ end frequencies of each comb
 start1=freqaxisp1(1);
 start2=freqaxisp2(1);
 finish1=freqaxisp1(end);
 finish2=freqaxisp2(end);
+%Find indices of start and end of overlap region
 over1=findEl(freqaxisp1,start2);
 over2=findEl(freqaxisp2,finish1);
+%Create array of total length equaling the overlap region plus the
+%remainder form each comb
 finallength=length(freqaxisp1)+length(freqaxisp2)-over2;
-freq=linspace(start1,finish2,finallength);
 spec1=amp1(1:over1-1);
-spec3=amp2(over2+1:end);
 spec2=amp1(over1:end)+amp2(1:over2);
+spec3=amp2(over2+1:end);
+% Returns the full spectrum along with an extended frequency axis
 spec=[spec1' spec2' spec3'];
+freq=linspace(start1,finish2,finallength);
--- Data_Analysis/UserTools/helperfuncs/findEl.m
+++ Data_Analysis/UserTools/helperfuncs/findEl.m
@@ -1,5 +1,15 @@
+% Quick and dirty function to find the element in an array. If the element
+% doesn't exist, the function returns the closest index in the array.
+% Written because Matlab's 'find' function wasn't working well and this
+% does. Takes in an array and a value to find in it and does basic out of
+% range error checking on the index
+
 function [index] = findEl (array, val)
 
-dd=array(2)-array(1);
+dd=array(2)-array(1); %step size in array
 start=array(1);
 index=round((val-start)/dd)+1;
+if (index > length(array) || index <= 0)
+    error('index is out of range, check value is contained in array')
+end
+    
