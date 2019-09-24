a5905686ca07a6fa8e211d486d9c5d0b01a98d7f
Martin Ritter <maritter@umd.edu>
2019-09-24 16:07:40 -0400

    Fixed findTones2 to subtract baseline from signal so that threshold can be relative to 0

--- Data_Analysis/UserTools/helperfuncs/findTones2.m
+++ Data_Analysis/UserTools/helperfuncs/findTones2.m
@@ -10,15 +10,8 @@ function [toneTimes] = findTones2(scopeData,verbose)
 if(nargin<2)
     verbose='false';
 end
-if(strcmp(verbose,'true')) 
-    tic
 
-    figure
-    hold on
-    plot(scopeData.y,'k')
-end
-
-threshold= 1e-3;        % cal. tone must not drop below this value
+threshold= 0.5e-3;        % cal. tone must not drop below this value
 pulsedur=1e-3;          % cal. tone is 1ms long
 dt=scopeData.x(2)-scopeData.x(1); % determine sampling rate of LeCroy
 width= cast(pulsedur/dt,'int32'); % approximate width of cal. tone
@@ -28,8 +21,20 @@ maxWidth= 1.25*width;
 toneTimes= [];          % array of cal. tone falling-edge times
 counter= 0;             % measures widths of threshold-exceeding signals
 
-for j=1:length(scopeData.y)
-    if scopeData.y(j)>threshold
+intTime=0.05;
+baseline=mean(scopeData.y(1:round(intTime/dt)));
+signal=scopeData.y-baseline;
+
+if(strcmp(verbose,'true')) 
+    tic
+
+    figure
+    hold on
+    plot(signal,'k')
+end
+
+for j=1:length(signal)
+    if signal(j)>threshold
         counter= counter+1;
     else
         if counter>minWidth && counter<maxWidth
@@ -37,9 +42,9 @@ for j=1:length(scopeData.y)
             % intervals immediately preceding and following the detected
             % interval was necessary. Without the rising edges of the probe
             % sweeps each trigger one or two detections.
-            avg= mean(scopeData.y(j-width:j));
-            if avg>mean(scopeData.y(j-2*width:j-width))&&...
-                    avg>mean(scopeData.y(j:j+width))
+            avg= mean(signal(j-width:j));
+            if avg>mean(signal(j-2*width:j-width))&&...
+                    avg>mean(signal(j:j+width))
                 toneTimes= [toneTimes;j];
             end
         end
@@ -48,8 +53,8 @@ for j=1:length(scopeData.y)
 end
 
 if(strcmp(verbose,'true'))
-    output= zeros(size(scopeData.y));
-    output(toneTimes)=1;
+    output= zeros(size(signal));
+    output(toneTimes)=max(signal);
     plot(output,'r')
     disp(toneTimes)
 
