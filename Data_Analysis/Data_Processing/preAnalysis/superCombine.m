bd55b53e1a779c5958a3d5761ff10984a058a5b4
Martin Ritter <maritter@umd.edu>
2019-10-09 18:52:43 -0400

    Updated superCombine script to reflect updated combine script

--- Data_Analysis/Data_Processing/preAnalysis/superCombine.m
+++ Data_Analysis/Data_Processing/preAnalysis/superCombine.m
@@ -1,10 +1,16 @@
-function []=superCombine(userpath)
+function []=superCombine(exptype, userpath)
 if(nargin<1)
-    userpath=pwd;
-    disp(['No directory provided, using current directory: ' pwd])
+    disp(['Using the current directory: ' pwd ])
+    pathOK=input('Is that ok (Y/N)?','s');
+    if(strcmp(pathOK,'Y'))
+        userpath=pwd;
+    else
+        userpath=input('Please enter the desired directory','s');
+    end
+    exptype=input('afc or echo data?','s');
 end
 cd(userpath)
 cd('0')
-combineFiles()
+combineFiles(exptype, fullfile(userpath, '0\'))
 cd('../1')
-combineFiles()
+combineFiles(exptype, fullfile(userpath, '1\'))
