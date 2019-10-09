eb1e2dc3e055524d716eff229cd5fd071353c8cf
Martin Ritter <maritter@umd.edu>
2019-10-09 18:42:02 -0400

    Make combineFiles more robust for parallelization by using full file paths instead of cd ing into directories. Also added user input instead of function arguments

--- Data_Analysis/Data_Processing/preAnalysis/combineFiles.m
+++ Data_Analysis/Data_Processing/preAnalysis/combineFiles.m
@@ -3,7 +3,8 @@
 %in the parent directory for further processing. It will process each
 %directory (i.e. modulation frequency) in parallel (max is 4 I think).
 
-function [] = combineFiles(userpath)
+function [] = combineFiles()
+%{
 if(nargin<1)
     userpath=pwd;
     disp(['No directory provided, using current directory: ' pwd])
@@ -11,6 +12,16 @@ end
 cd (userpath)
 [~, date] = fileparts(userpath);
 disp(['Processing data from: ' date])
+%}
+disp(['Using the current directory: ' pwd ])
+pathOK=input('Is that ok (Y/N)?','s');
+if(strcmp(pathOK,'Y'))
+    userpath=pwd;
+else
+    userpath=input('Please enter the desired directory','s');
+end
+
+exptype=input('afc or echo data?','s');
 
 %% Getting all the folder names from the directory
 files=dir; %lists all the file and folder names
@@ -18,18 +29,38 @@ files=files(~ismember({files.name},{'.','..'})); %removes the 'current' and 'par
 dirFlags=[files.isdir]; %flags each entry as a folder (boolean)
 folders=files(dirFlags); %only store the folders
 
+[~, idx]=sort_nat({folders.name});
+folders=folders(idx); %sorts the folders by frequency
+
 %% Loops through all the folders and processes the data
 parfor k=1:length(folders)
     datafolder=folders(k).name; %get name of current folder
     disp(['Current data folder: ' datafolder])
-    cd(datafolder); %cd into current folder where the data is stored 
+    %cd(datafolder); %cd into current folder where the data is stored 
     
     %get the various datafiles
-    ofcdat=dir('ofc*.txt'); %Optical comb
-    rfcdat=dir('rfc*.txt'); %RF comb
-    odatC1=dir('C1*.trc'); %AFC comb
-    odatC3=dir('C3*.trc'); %Probe
+    %ofcdat=dir('ofc*.txt'); %Optical comb
+    %rfcdat=dir('rfc*.txt'); %RF comb
+    %odatC1=dir('C1*.trc'); %AFC comb
+    %odatC3=dir('C3*.trc'); %Probe
     
+    filepath=fullfile(userpath, datafolder, 'ofc*.txt');
+    ofcdat=dir(filepath);
+    filepath=fullfile(userpath, datafolder, 'rfc*.txt');
+    rfcdat=dir(filepath);
+    filepath=fullfile(userpath, datafolder, 'C1*.trc');
+    odatC1=dir(filepath);
+    filepath=fullfile(userpath, datafolder, 'C3*.trc');
+    odatC3=dir(filepath);
+    
+    for i=1:length(ofcdat)
+        ofcdat(i).name=fullfile(userpath, datafolder, ofcdat(i).name);
+        rfcdat(i).name=fullfile(userpath, datafolder, rfcdat(i).name);
+    end
+    for i=1:length(odatC1)
+        odatC1(i).name=fullfile(userpath, datafolder, odatC1(i).name);
+        odatC3(i).name=fullfile(userpath, datafolder, odatC3(i).name);
+    end
     datafiles=cell(4,3); %create a cell to hold all the data
     datafiles{1,1}=ofcdat;
     datafiles{2,1}=rfcdat;
@@ -45,10 +76,12 @@ parfor k=1:length(folders)
             datafiles{i,2}=[xaxis;yaxis]; %store data in corresponding cell
         end
     end
-
-    cd .. %move up a directory to store files
+    if(length(xaxis)==1)
+        continue;
+    end
+    %cd .. %move up a directory to store files
     tooth=str2double(datafolder)/1e+6; %extract the tooth spacing for filenaming
-    types={'ofc','rfc','afc','probe'};
+    types={'ofc','rfc',exptype,'probe'};
     fileID=zeros(1,4);
     %Store a .txt file for each dataset
     for i=1:4
