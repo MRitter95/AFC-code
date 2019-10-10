efe2d3f8a73a1243213a581be6a454dbd70d755c
Martin Ritter <maritter@umd.edu>
2019-10-10 16:31:06 -0400

    Added input arguments to specify range of data to be analyzed

--- Data_Analysis/UserTools/helperfuncs/takeDiff.m
+++ Data_Analysis/UserTools/helperfuncs/takeDiff.m
@@ -1,7 +1,7 @@
 %This function generates an array of the variations between two consecutive
 %traces
 
-function [truedif, diffs, normvar] = takeDiff(data)
+function [truedif, diffs, normvar] = takeDiff(data, start, stop)
 
 if(isempty(data))
     disp('no data of this type, returning default values')
@@ -9,6 +9,15 @@ if(isempty(data))
     truedif=0;
     normvar=0;
     return 
+end
+
+size=ReadLeCroyBinaryWaveform(data(1).name); %get size of data files
+if(nargin<2)
+    start=1;
+    stop=length(size.x);
+end
+if(nargin<3)
+    stop=length(size.x);
 end
 
 numfiles=length(data);
@@ -19,11 +28,10 @@ normvar=zeros(1,numfiles-1);
 for i=1:numfiles-1
     current=ReadLeCroyBinaryWaveform(data(i).name);
     next=ReadLeCroyBinaryWaveform(data(i+1).name);
-    truedif(i)=sum((next.y-current.y).^2)/length(current.x);
-    stop=500;
-    normvar(i)=sum((current.y(2:stop+1)-current.y(1:stop)).^2)/stop;
+    truedif(i)=sum((next.y(start:stop)-current.y(start:stop)).^2)/length(current.x(start:stop));
+    stopval=500;
+    normvar(i)=sum((current.y(2:stopval+1)-current.y(1:stopval)).^2)/stopval;
     diffs(i)=truedif(i)-normvar(i);
 end
 
 end
-    
