60268e9c13e2693821ce36dcf49a2a16d6184ec0
Martin Ritter <maritter@umd.edu>
2019-10-10 17:13:52 -0400

    Added comments and structure to scripts

--- Data_Analysis/Data_Processing/preAnalysis/plotdifs.m
+++ Data_Analysis/Data_Processing/preAnalysis/plotdifs.m
@@ -1,30 +1,42 @@
+%This script calculates the difference between consecutive traces for all
+%frequencies in an experiment and stores the results in three cells that
+%can be plotted with the plotdifplotter script if saved. 
+
+%% Get list of folders and sort them
 files=dir; %lists all the file and folder names
-files=files(~ismember({files.name},{'.','..'})); %removes the 'current' and 'parent' directory folders
+%removes the 'current' and 'parent' directory folders
+files=files(~ismember({files.name},{'.','..'})); 
 dirFlags=[files.isdir]; %flags each entry as a folder (boolean)
 folders=files(dirFlags); %only store the folders
-
-[~,idx]=sort_nat({folders.name});
+%sort folders currently by numerical value
+[~,idx]=sort_nat({folders.name}); 
 folders=folders(idx);
+
+%% Initialization
 difference=cell(1, length(folders));
 netval=cell(1, length(folders));
 variance=cell(1, length(folders));
-
 topdir=pwd;
 
-parfor k=1:length(folders)
-    currentdir=folders(k).name;
+parfor k=1:length(folders) %loop over all the folders in the directory
+    currentdir=folders(k).name; %get current folder name
     disp(['Current data folder: ' currentdir])
+    %create full file path to files that will be processed
     filepath=fullfile(topdir, currentdir, 'C1*.trc');
     afcs=dir(filepath);
     for i=1:length(afcs)
+        %add directory info to dir output so we don't have to CD into the
+        %folder
         afcs(i).name=fullfile(topdir, currentdir, afcs(i).name);
     end
-    
+    % Initialize arrays (required when using parfor apparently)
     tempdiff=zeros(1, length(afcs));
     tempnet=zeros(1, length(afcs));
     tempvar=zeros(1, length(afcs));
     
     [tempdiff, tempnet, tempvar]=takeDiff(afcs);
+    
+    %store results in cell
     difference{k}=tempdiff;
     netval{k}=tempnet;
     variance{k}=tempvar;
