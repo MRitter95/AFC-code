c839c6b07995bf7695bd0765d56b1982607a45ec
Martin Ritter <maritter@umd.edu>
2019-10-14 11:47:33 -0400

    Aesthetics and updated to store diff between ith trace and the last trace in the set

--- Data_Analysis/Data_Processing/preAnalysis/plotdifs.m
+++ Data_Analysis/Data_Processing/preAnalysis/plotdifs.m
@@ -3,41 +3,46 @@
 %can be plotted with the plotdifplotter script if saved. 
 
 %% Get list of folders and sort them
-files=dir; %lists all the file and folder names
+files    = dir; %lists all the file and folder names
 %removes the 'current' and 'parent' directory folders
-files=files(~ismember({files.name},{'.','..'})); 
-dirFlags=[files.isdir]; %flags each entry as a folder (boolean)
-folders=files(dirFlags); %only store the folders
+files    = files(~ismember({files.name},{'.','..'})); 
+dirFlags = [files.isdir]; %flags each entry as a folder (boolean)
+folders  = files(dirFlags); %only store the folders
 %sort folders currently by numerical value
-[~,idx]=sort_nat({folders.name}); 
-folders=folders(idx);
+[~,idx]  = sort_nat({folders.name}); 
+folders  = folders(idx);
 
 %% Initialization
-difference=cell(1, length(folders));
-netval=cell(1, length(folders));
-variance=cell(1, length(folders));
-topdir=pwd;
+difference = cell(1, length(folders));
+difflast   = cell(1, length(folders));
+netval     = cell(1, length(folders));
+variance   = cell(1, length(folders));
+topdir     = pwd;
 
 parfor k=1:length(folders) %loop over all the folders in the directory
     currentdir=folders(k).name; %get current folder name
     disp(['Current data folder: ' currentdir])
     %create full file path to files that will be processed
-    filepath=fullfile(topdir, currentdir, 'C1*.trc');
-    afcs=dir(filepath);
+    filepath = fullfile(topdir, currentdir, 'C1*.trc');
+    afcs     = dir(filepath);
+    
     for i=1:length(afcs)
         %add directory info to dir output so we don't have to CD into the
         %folder
         afcs(i).name=fullfile(topdir, currentdir, afcs(i).name);
     end
-    % Initialize arrays (required when using parfor apparently)
-    tempdiff=zeros(1, length(afcs));
-    tempnet=zeros(1, length(afcs));
-    tempvar=zeros(1, length(afcs));
     
-    [tempdiff, tempnet, tempvar]=takeDiff(afcs);
+    % Initialize arrays (required when using parfor apparently)
+    tempdiff     = zeros(1, length(afcs));
+    tempdifflast = zeros(1,length(afcs));
+    tempnet      = zeros(1, length(afcs));
+    tempvar      = zeros(1, length(afcs));
+    
+    [tempdiff, tempdifflast, tempnet, tempvar]=takeDiff(afcs);
     
     %store results in cell
-    difference{k}=tempdiff;
-    netval{k}=tempnet;
-    variance{k}=tempvar;
+    difference{k} = tempdiff;
+    difflast{k}   = tempdifflast;
+    netval{k}     = tempnet;
+    variance{k}   = tempvar;
 end
