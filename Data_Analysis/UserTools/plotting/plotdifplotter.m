5a7f41ab424c3fd3ad5eff7c1c3433795e478468
Martin Ritter <maritter@umd.edu>
2019-10-14 11:44:03 -0400

    Added comments and formatted code

--- Data_Analysis/UserTools/plotting/plotdifplotter.m
+++ Data_Analysis/UserTools/plotting/plotdifplotter.m
@@ -1,9 +1,21 @@
-for i=1:20
-figure(i)
-hold on
-for j=1:10
-plot(difference{j+(i-1)*10}(1:end-1))
-end
-title(sprintf('Starting at %.1f MHz going to %.1f MHz',(i-1)+0.1,i))
-legend({folders(1+10*(i-1):10+10*(i-1)).name})
+%This script plots the output from the plotdifs script. The script expects
+%the workspace to be saved from the result of the computation (this could
+%probably be improved but it's how it works for now)
+
+%date= '10 Oct;';
+
+for i=1:20 %Go through 20 frequencies
+    
+    for j=1:10 %Show slices of 1MHz at a time
+        %depending on data, might want to skip first or last entry
+        figure(2*i)
+        hold on
+        plot(log10(difference{j+(i-1)*10}(1:end-1))) 
+        figure(2*i+1)
+        hold on
+        plot(log10(difflast{j+(i-1)*10}(1:end-1))) 
+    end
+    title(sprintf(strcat(date,' Starting at %.1f MHz going to %.1f MHz'),(i-1)+0.1,i))
+    axis([-inf,inf,-inf,inf])
+    legend({folders(1+10*(i-1):10+10*(i-1)).name})
 end
--- Data_Analysis/UserTools/plotting/quickscopeplotter.m
+++ Data_Analysis/UserTools/plotting/quickscopeplotter.m
@@ -1,3 +1,7 @@
+%This quick script will plot all the C1 traces with a given name and show
+%them on the same plot. Should probably be changed to a function in the
+%longer term
+
 files=dir('C1*.trc');
 figure
 hold on
