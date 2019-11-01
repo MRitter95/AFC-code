de8a37fc0370429e21581092874bc9b386fa97c1
Martin Ritter <maritter@umd.edu>
2019-11-01 17:07:00 -0400

    Made RFcaller more flexible and calleable from command line

--- Modeling/RFcaller.m
+++ Modeling/RFcaller.m
@@ -1,10 +1,32 @@
 % Script to call RFpoweranalysis on all the folders
 % Needs to be modified to work in arbitrary folders
-threshold=-70;
-[modfreqs10, power10]=RFpoweranalysis('10MHz/0', threshold, 'true');
-[modfreqs20, power20]=RFpoweranalysis('20MHz/0', threshold, 'true');
-[modfreqs30, power30]=RFpoweranalysis('30MHz/0', threshold, 'true');
-[modfreqs40, power40]=RFpoweranalysis('40MHz/0', threshold, 'true');
+
+function [] = RFcaller(userpath, threshold, isRFC)
+
+if(nargin<1)
+    disp(['Using the current directory: ' pwd ])
+    pathOK=input('Is that ok (Y/N)?','s');
+    if(strcmp(pathOK,'Y'))
+        userpath=pwd;
+    else
+        userpath=input('Please enter the desired directory','s');
+    end
+    RFCstat=input('rfc data? (Y/N)','s');
+    if(strcmp(RFCstat,'Y'))
+        isRFC='true';
+    else
+        isRFC='false';
+    end
+    threshold=input('Threshold');
+end
+
+disp(['Processing data from: ' userpath])
+cd (userpath)
+
+[modfreqs10, power10]=RFpoweranalysis('10MHz/0', threshold, isRFC);
+[modfreqs20, power20]=RFpoweranalysis('20MHz/0', threshold, isRFC);
+[modfreqs30, power30]=RFpoweranalysis('30MHz/0', threshold, isRFC);
+[modfreqs40, power40]=RFpoweranalysis('40MHz/0', threshold, isRFC);
 
 figure
 hold on
