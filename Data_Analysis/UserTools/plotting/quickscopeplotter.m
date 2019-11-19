72ee399bab25251d2de5ce99e3adfc412cc0682f
Martin Ritter <maritter@umd.edu>
2019-11-19 16:01:20 -0500

    Updated and commented plotting functions

--- Data_Analysis/UserTools/plotting/quickscopeplotter.m
+++ Data_Analysis/UserTools/plotting/quickscopeplotter.m
@@ -1,6 +1,23 @@
 %This quick script will plot all the C1 traces with a given name and show
-%them on the same plot. Should probably be changed to a function in the
-%longer term
+%them on the same plot. 
+%Input: userpath, path to traces
+%Input: step, vertical shift used to plot consecutive traces
+%Output: none, generates a figure
+
+function [] = quickscopeplotter (userpath, step)
+
+%% Get user input
+if(nargin<1)
+    disp(['Using the current directory: ' pwd])
+    pathOK       = input('Is that ok (Y/N)?','s');
+    if(strcmp(pathOK,'Y') || strcmp(pathOK,'y'))
+        userpath = pwd;
+    else
+        userpath = input('Please enter the desired directory','s');
+    end
+    step         = input('Step size (vertical shift on plots)');
+end
+disp(['Processing data from: ' userpath])
 
 files=dir('C1*.trc');
 figure
--- Data_Analysis/UserTools/plotting/specDraw.m
+++ Data_Analysis/UserTools/plotting/specDraw.m
@@ -1,6 +1,12 @@
+%Simple function to plot RSA data (saved in .txt format)
+%Input: inputfile, .txt file with RSA data
+%Input: startfreq, starting frequency of RSA sweep
+%Input: endfreq, ending frequency of RSA sweep
+
 function [] = specDraw(inputfile, startfreq, endfreq)
-infile=fopen(inputfile);
-format='%f';
-logamps=fscanf(infile,format);
-freq=linspace(startfreq, endfreq, length(logamps));
+
+format  = '%f';
+infile  = fopen(inputfile);
+logamps = fscanf(infile,format);
+freq    = linspace(startfreq, endfreq, length(logamps));
 plot(freq, logamps)
