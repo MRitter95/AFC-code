56a607bf6ea93215855bad5ff1d717c0daeefd45
Martin Ritter <maritter@umd.edu>
2019-12-12 13:37:58 -0500

    Added helper function to read pressure from cavity

---
+++ Data_Analysis/UserTools/helperfuncs/pressureRead.m
@@ -0,0 +1,13 @@
+format ='%d-%d-%d %d:%d:%f\t%f';
+fID=fopen('2019-12-06.txt');
+rawdata=fscanf(fID, format, [7,Inf]);
+
+pressure=rawdata(7,:);
+init=rawdata(:,1);
+mins=60;
+hours=60*mins;
+days=24*hours;
+months=30*days;
+years=365*days;
+time=(rawdata(1,:)-init(1))*years+(rawdata(2,:)-init(2))*months+(rawdata(3,:)...
+    -init(3))*days+(rawdata(4,:)-init(4))*hours+(rawdata(5,:)-init(5))*mins+(rawdata(6,:)-init(6));
