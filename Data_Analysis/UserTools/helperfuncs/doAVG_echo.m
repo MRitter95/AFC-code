1389e762d30bb92598608e3858383ce14f7817ba
aditya36 <aditya36@gmail.com>
2020-03-05 15:19:54 -0500

    finished writing program

--- Data_Analysis/UserTools/helperfuncs/doAVG_echo.m
+++ Data_Analysis/UserTools/helperfuncs/doAVG_echo.m
@@ -9,54 +9,67 @@
 %Output: xint, xoff, time step and time offset of LeCroy samples
 %Output: ygain, yoff, scale factor and y offset for LeCroy amplitudes
 
-function [xaxis, yaxis, xint, xoff, ygain, yoff] = doAVG_echo(echoDat, normDat, RSArange, numtoskip)
+function [xaxis,normAxis,echoAxis,xInt,xOff,normGain,normOff,...
+    echoGain,echoOff] = doAVG_echo(normDat,echoDat,numtoskip)
 
 numfiles = length(echoDat); %get number of files to process
 
-if(isempty(data))
+if(isempty(echoDat))
     disp('no data of this type, returning default values')
-    yaxis = 0;
+    normAxis = 0;
     xaxis = 0;
-    xint  = 1; %x time step
-    xoff  = 0; %x offset
-    ygain = 1; %y gain
-    yoff  = 0; %y offset
+    xInt  = 1; %x time step
+    xOff  = 0; %x offset
+    normGain = 1; %y gain
+    normOff  = 0; %y offset
     return 
 end
 
-[~, ~, ext] = fileparts(echoDat(1).name); %get extension to choose which method to use
-
-yaxis = 0;
+normAxis = 0;
 xaxis = 0;
-xint  = 1; %x time step
-xoff  = 0; %x offset
-ygain = 1; %y gain
-yoff  = 0; %y offset
+xInt  = 1; %x time step
+xOff  = 0; %x offset
+normGain = 1; %y gain
+normOff  = 0; %y offset
 
 % the new part. we find the peak in each probe data set.
-offsetTrace= [];
+offsetNorm= [];
+offsetEcho= [];
 peaks= zeros(numfiles-numtoskip,1);
 for k=numtoskip+1:numfiles
-    [wave, xint, xoff, ygain, yoff] = ReadLeCroyBinaryWaveformExtra(data(k).name);
+    [wave,~,~,...
+        normGain,normOff]= ReadLeCroyBinaryWaveformExtra(normDat(k).name);
     
     [pk,loc] = findpeaks(wave.y,'MinPeakDistance',1e-5);
     blah= sortrows([pk,loc],'descend');
     
-    peaks(k)= loc;
+    peaks(k)= blah(1,2);
+    offsetNorm= [offsetNorm,wave.y];
+    
+    [wave,xInt,xOff,...
+        echoGain,echoOff]= ReadLeCroyBinaryWaveformExtra(echoDat(k).name);
+    offsetEcho= [offsetEcho,wave.y];
+    
     time= wave.x;
-    offsetTrace= [offsetTrace,wave.y];
 end
 
 % now instead of simply averaging, we first align all the traces in time.
-alignedTrace= [zeros( max(peaks)-min(peaks),numfiles-numtoskip );...
-    zeros(size(offsetTrace))];
+alignedNorm= [zeros( max(peaks)-min(peaks),numfiles-numtoskip );...
+    zeros(size(offsetNorm))];
+alignedEcho= alignedNorm;
+
 for k=numtoskip+1:numfiles
-    baseline= mean(alignedTrace(:,k);
-    alignedTrace(:,k)= [baseline*ones( max(peaks)-peaks(k),1 );...
-        offsetTrace(:,k)];
+    baselineNorm= mean(offsetNorm(:,k));
+    alignedNorm(:,k)= [baselineNorm*ones( max(peaks)-peaks(k),1 );...
+        offsetNorm(:,k)];
+    
+    baselineEcho= mean(offsetEcho(:,k));
+    alignedEcho(:,k)= [baselineEcho*ones( max(peaks)-peaks(k),1 );...
+        offsetNorm(:,k)];
 end
 
-yaxis= mean(offsetTrace,2);
+normAxis= mean(offsetNorm,2);
+echoAxis= mean(offsetEcho,2);
 
 % we have to pad the x axis as well.
 xaxis= [time',linspace(time(end),...
@@ -64,5 +77,6 @@ xaxis= [time',linspace(time(end),...
     max(peaks)-min(peaks))];
 
 %convert y values back to int16 to store more efficiently  
-yaxis = int16((yaxis'+yoff)/ygain);
+normAxis= int16((normAxis'+normOff)/normGain);
+echoAxis= int16((echoAxis'+echoOff)/echoGain);
  
